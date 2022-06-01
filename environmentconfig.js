83da85a803886c479aa951ebf6296c1b057bcb96
Jitali-Patel <jitali.alphabin@gmail.com>
2022-06-01 14:27:12 +0530

    Added envconfig file

--- environmentconfig.js
+++ environmentconfig.js
@@ -1,8 +1,45 @@
-export function userConfig(filename) {
+ export function userManegementConfig(filename) {
     let envConfig = {
-      BASE_URL: "http://todo-app-barkend.herokuapp.com/todos/",
-      ENV: "DEVELOPMENT",
+      BASE_URL: "https://delta-dev.finzipp.com/API/UserManagement",
+    };
+    return Object.assign({}, envConfig, filename);
+  }
+
+  export function masterDataConfig(filename) {
+    let envConfig = {
+      BASE_URL: "https://delta-dev.finzipp.com/API/MasterData",
+    };
+    return Object.assign({}, envConfig, filename);
+  }
+
+  export function transactionConfig(filename) {
+    let envConfig = {
+      BASE_URL: "https://delta-dev.finzipp.com/API/Transaction",
+    };
+    return Object.assign({}, envConfig, filename);
+  }
+
+  export function reportingConfig(filename) {
+    let envConfig = {
+      BASE_URL: "https://delta-dev.finzipp.com/API/Reporting",
+    };
+    return Object.assign({}, envConfig, filename);
+  }
+
+  export function orderExecutionConfig(filename) {
+    let envConfig = {
+      BASE_URL: "https://delta-dev.finzipp.com/API/OrderExecution",
+    };
+    return Object.assign({}, envConfig, filename);
+  }
+
+  export function loggingConfig(filename) {
+    let envConfig = {
+      BASE_URL: "https://delta-dev.finzipp.com/API/Logging",
     };
     return Object.assign({}, envConfig, filename);
   }
   
+  
+  
+
