02e7620a22c0359e40e00dc6229b3bc8af3c350d
Jitali-Patel <jitali.alphabin@gmail.com>
2022-06-03 19:05:47 +0530

    Modified report file

--- helpers/report_generater.js
+++ helpers/report_generater.js
@@ -2,8 +2,9 @@ import { htmlReport } from "../dist/bundle.js";
 import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export function handleSummary(data) {
-    return {
-      '../test-results/reports/result.html': htmlReport(data, { debug: false }),
+     return {
+      '../../../../test-results/reports/result.html': htmlReport(data, { debug: false }),
       stdout: textSummary(data, { indent: ' ', enableColors: true }),
     }
 }
+
